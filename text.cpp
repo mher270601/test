7dc45287961a9e4bfb3a924185da8b02789a449a
mher270601 <41270907+mher270601@users.noreply.github.com>
2021-11-13 16:16:12 +0400

    Text

---
+++ text.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+
+   std::cout << "        CCCCCCCCCCCCC                 lllllll                                           FFFFFFFFFFFFFFFFFFFFFF  iiii  lllllll lllllll 
+" << std::endl;
+   std::cout << "     CCC::::::::::::C                 l:::::l                                           F::::::::::::::::::::F i::::i l:::::l l:::::l 
+" << std::endl;
+   std::cout << "   CC:::::::::::::::C                 l:::::l                                           F::::::::::::::::::::F  iiii  l:::::l l:::::l 
+" << std::endl;
+   std::cout << "  C:::::CCCCCCCC::::C                 l:::::l                                           FF::::::FFFFFFFFF::::F        l:::::l l:::::l 
+" << std::endl;
+   std::cout << " C:::::C       CCCCCC   ooooooooooo    l::::l    ooooooooooo   rrrrr   rrrrrrrrr          F:::::F       FFFFFFiiiiiii  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C               oo:::::::::::oo  l::::l  oo:::::::::::oo r::::rrr:::::::::r         F:::::F             i:::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C              o:::::::::::::::o l::::l o:::::::::::::::or:::::::::::::::::r        F::::::FFFFFFFFFF    i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C              o:::::ooooo:::::o l::::l o:::::ooooo:::::orr::::::rrrrr::::::r       F:::::::::::::::F    i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C              o::::o     o::::o l::::l o::::o     o::::o r:::::r     r:::::r       F:::::::::::::::F    i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C              o::::o     o::::o l::::l o::::o     o::::o r:::::r     rrrrrrr       F::::::FFFFFFFFFF    i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "C:::::C              o::::o     o::::o l::::l o::::o     o::::o r:::::r                   F:::::F              i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << " C:::::C       CCCCCCo::::o     o::::o l::::l o::::o     o::::o r:::::r                   F:::::F              i::::i  l::::l  l::::l 
+" << std::endl;
+   std::cout << "  C:::::CCCCCCCC::::Co:::::ooooo:::::ol::::::lo:::::ooooo:::::o r:::::r                 FF:::::::FF           i::::::il::::::ll::::::l
+" << std::endl;
+   std::cout << "   CC:::::::::::::::Co:::::::::::::::ol::::::lo:::::::::::::::o r:::::r                 F::::::::FF           i::::::il::::::ll::::::l
+" << std::endl;
+   std::cout << "     CCC::::::::::::C oo:::::::::::oo l::::::l oo:::::::::::oo  r:::::r                 F::::::::FF           i::::::il::::::ll::::::l
+" << std::endl;
+   std::cout << "        CCCCCCCCCCCCC   ooooooooooo   llllllll   ooooooooooo    rrrrrrr                 FFFFFFFFFFF           iiiiiiiillllllllllllllll
+" << std::endl;
+   return 0;
+}
